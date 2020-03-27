889b0beb81014d87ef2829afebf7fff6e602d1d9
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2020-03-27 14:01:09 +0900

    Setting Spring Boot

---
+++ Restaurant-api/src/main/java/interfaces/WelcomeController.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public  String Hello(){
+        return "Hello World";
+    }
+}
