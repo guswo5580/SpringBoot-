aba4e405f95cdb7cfa98d3f36a16650d03ba3f18
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2020-03-27 14:32:44 +0900

    Modify Folder Directory

--- Restaurant-api/src/main/java/Study/SpringBoot/Restaurant/interfaces/WelcomeController.java
+++ Restaurant-api/src/main/java/Study/SpringBoot/Restaurant/interfaces/WelcomeController.java
@@ -1,4 +1,4 @@
-package interfaces;
+package Study.SpringBoot.Restaurant.interfaces;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Restaurant-api/src/test/java/Study/SpringBoot/Restaurant/RestaurantApplicationTests.java
+++ Restaurant-api/src/test/java/Study/SpringBoot/Restaurant/RestaurantApplicationTests.java
@@ -2,6 +2,7 @@ package Study.SpringBoot.Restaurant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class RestaurantApplicationTests {
