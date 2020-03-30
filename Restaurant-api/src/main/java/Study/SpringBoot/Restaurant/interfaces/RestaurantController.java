e7725d34607e5e58760f8695c348885d7733eca2
DESKTOP-JIU76K2\chj <guswo5580@gmail.com>
2020-03-30 15:28:47 +0900

    Add Detail Router using Dynamic

--- Restaurant-api/src/main/java/Study/SpringBoot/Restaurant/interfaces/RestaurantController.java
+++ Restaurant-api/src/main/java/Study/SpringBoot/Restaurant/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package Study.SpringBoot.Restaurant.interfaces;
 
 import Study.SpringBoot.Restaurant.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import java.util.List;
 public class RestaurantController {
 
     @GetMapping("/restaurants")
+    //이 list 함수는 /restaurants로 접근하는 도메인에 나타낼 내용을 정의할 거야
     public List<Restaurant> list(){
         List<Restaurant> restaurants = new ArrayList<Restaurant>();
 
@@ -20,4 +22,21 @@ public class RestaurantController {
 
         return restaurants;
     }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id){
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(1005L, "Bob zip2", "Seoul1"));
+        restaurants.add(new Restaurant(1006L, "Bob zip3", "Seoul2"));
+
+        Restaurant restaurant = restaurants.stream()
+                .filter(v -> v.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return restaurant;
+    }
+
 }
--- Restaurant-api/src/test/java/Study/SpringBoot/Restaurant/interfaces/RestaurantControllerTest.java
+++ Restaurant-api/src/test/java/Study/SpringBoot/Restaurant/interfaces/RestaurantControllerTest.java
@@ -14,15 +14,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RestaurantController.class) //RestaurantController 클래스를 Test 하겠다는 의미
-
 class RestaurantControllerTest {
-    @Autowired
+    @Autowired //따로 실행하지 않아도 자동으로 구동하겠다
     private MockMvc mvc;
 
     @Test //Test 코드임을 명시
+    //restaurants로 접근하는 도메인은 다음을 검증할거야 -- 도메인의 내용은 RestaurantController 안에 있어
     public void list() throws Exception { //perform에 대한 예외처리
         //mvc는 field를 이용해 선언
         mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk());
+                //.andExpect((ResultMatcher) content().string(containsString("\"name\" : \"Bob zip\"")));
+    }
+
+    @Test
+    //restaurants/1로 접근하는 도메인은 다음을 검증할거야 -- 도메인의 내용은 RestaurantController 안에 있어
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1"))
                 .andExpect(status().isOk());
     }
 }
